e26777fbce12f45c81f1581521c054e7c2cfc695
Kelly Ruan <kruan@bu.edu>
2020-11-30 21:57:36 -0500

    fixed bugs. calling generate() will generate all csv files in a folder called data

--- transit-equity/datasets/generate.py
+++ transit-equity/datasets/generate.py
@@ -43,7 +43,7 @@ def get_ridership_data() -> pd.DataFrame:
     RTA_RIDERSHIP_DATA = requests.get(RTA_RIDERSHIP_URL)
     ridership_df = pd.read_excel(BytesIO(RTA_RIDERSHIP_DATA.content), sheet_name='MASTER')
     ridership_df = ridership_df[ridership_df.Mode == 'MB'] # only buses
-    return ridership_df[ridership_df['HQ State'] == 'MA'] # only buses
+    return ridership_df[ridership_df['HQ State'] == 'MA'] # only in MA
 
 
 def get_tract_population_data() -> pd.DataFrame:
@@ -66,87 +66,86 @@ def get_county_population_data() -> pd.DataFrame:
 
 def map_stops_to_routes():
     """Add stops to routes"""
-	# Filepath
-	bus_routes_fp = "../RTA_Bus_Routes-shp/RTA_Bus_Routes.shp"
-	bus_stops_fp = "../RTA_Bus_Stops-shp/RTA_Bus_Stops.shp"
+    # Filepath
+    bus_routes_fp = "RTA_Bus_Routes-shp/RTA_Bus_Routes.shp"
+    bus_stops_fp = "RTA_Bus_Stops-shp/RTA_Bus_Stops.shp"
 
-	# Read the data
-	bus_routes_df = gpd.read_file(bus_routes_fp)
-	bus_stops_df = gpd.read_file(bus_stops_fp)
+    # Read the data
+    bus_routes_df = gpd.read_file(bus_routes_fp)
+    bus_stops_df = gpd.read_file(bus_stops_fp)
 
-	# Pick specific columns
-	routes = bus_routes_df[['OBJECTID','geometry','route_id', 'route_shor', 'route_long']]
-	stops = bus_stops_df[['OBJECTID','geometry','stop_id']]
+    # Pick specific columns
+    routes = bus_routes_df[['OBJECTID','geometry','route_id', 'route_shor', 'route_long']]
+    stops = bus_stops_df[['OBJECTID','geometry','stop_id']]
 
-	# map bus stops to routes
-	mapped_routes = [-1] * len(stops)
+    # map bus stops to routes
+    mapped_routes = [-1] * len(stops)
 
-	for index, row in routes.iterrows():
-	    line = row['geometry']
-	    for index2, row2 in stops.iterrows():
-	        stop = row2['geometry']
+    for index, row in routes.iterrows():
+        line = row['geometry']
+        for index2, row2 in stops.iterrows():
+            stop = row2['geometry']
 
-	        if line.distance(stop) < 100:
-	            mapped_routes[index2] = row['route_id']
+            if line.distance(stop) < 100:
+                mapped_routes[index2] = row['route_id']
 
-	stops['route_id'] = mapped_routes
-    stops.to_csv('../data/bus_stop_route_mapping.csv')
-	return stops # stops.to_csv('../data/bus_stop_route_mapping.csv')
+    stops['route_id'] = mapped_routes
+    stops.to_csv('data/bus_stop_route_mapping.csv')
+    return stops 
 
 
 def get_joined_data():
     """Returns bus stop area household income data attached with bus routes"""
-    income = pd.read_csv('../data/bus_area_income_ma.csv', index_col=0).drop(columns=['route'])
-    stop_route_map = pd.read_csv('../data/bus_stop_route_mapping.csv', index_col=0)
+    income = pd.read_csv('data/rta_bus_stop_income_ma.csv', index_col=0)
+    stop_route_map = pd.read_csv('data/bus_stop_route_mapping.csv', index_col=0)
     result = pd.merge(income, stop_route_map, on='stop_id', how='inner')
-    result.to_csv('../data/result.csv') 
     return result
 
 
 def generate():
     """Generates all datasets concurrently"""
-    if not os.path.exists('../data'):
-        raise Exception('Missing ../data/bus_area_income.csv xor ../data/tl_2019_25_tract.shp')
+    if not os.path.exists('data'):
+        os.makedirs('data')
 
     route_df = get_route_data()
-    route_df = route_df.drop(columns=['route_desc', 'route_color', 'route_text_color', 'route_sort_order', 'min_headway_minutes', 'eligibility_restricted', 'continuous_pickup', 'continuous_drop_off', 'route_type_text'])
+    route_df = route_df.drop(columns=['route_type', 'route_desc', 'route_color', 'route_text_color', 'route_sort_order', 'min_headway_minutes', 'eligibility_restricted', 'continuous_pickup', 'continuous_drop_off', 'route_type_text'])
     
-    population_df = get_tract_population_data()
+    tract_population_df = get_tract_population_data()
 
     ridership_df = get_ridership_data()
     ridership_df = ridership_df[['5 digit NTD ID', 'Agency', 'Service Area Population', 'TOS', 'Active', 'Passenger Miles FY', 'Unlinked Passenger Trips FY', 'Fares FY', 'Operating Expenses FY', 'Average Cost per Trip FY', 'Average Fares per Trip FY']]
 
-    county_df = get_county_population_data()
+    county_population_df = get_county_population_data()
 
     # join population_df to bus_area_income_df
-    bus_area_income_df = get_bus_stop_data()
-    bus_area_income_df = pd.merge(bus_area_income_df, population_df, how='inner', left_on='census_tract', right_on='tract')
+    bus_stop_income_df = get_bus_stop_data()
+    bus_stop_income_df = pd.merge(bus_stop_income_df, tract_population_df, how='inner', left_on='census_tract', right_on='tract')
     # drop negative income data
-    bus_area_income_df['median_household_income'] = bus_area_income_df['median_household_income'].astype(float)
-    bus_area_income_df = bus_area_income_df[bus_area_income_df.median_household_income > 0] 
-    bus_area_income_df.dropna()
+    bus_stop_income_df['median_household_income'] = bus_stop_income_df['median_household_income'].astype(float)
+    bus_stop_income_df = bus_stop_income_df[bus_stop_income_df.median_household_income > 0] 
+    bus_stop_income_df.dropna()
     # rename population column
-    bus_area_income_df = bus_area_income_df.rename(columns={"B00001_001E": "population"})
+    bus_stop_income_df = bus_stop_income_df.rename(columns={"B00001_001E": "population"})
     # drop duplicate bus stops
-    bus_area_income_df = bus_area_income_df.drop_duplicates(subset=['stop_id'])
-    # drop unnecessary columns
-    bus_area_income_df = bus_area_income_df.drop(columns=['platform_code', 'zone_id', 'stop_timezone', 'position', 'direction', 'state', 'stop_desc'])
+    bus_stop_income_df = bus_stop_income_df.drop_duplicates(subset=['stop_id'])
+    # drop unnecessary columns and rows
+    bus_stop_income_df = bus_stop_income_df.drop(columns=['platform_code', 'zone_id', 'stop_timezone', 'position', 'direction', 'state', 'stop_desc'])
+    ridership_df = ridership_df.loc[ridership_df['Agency'] != 'Massachusetts Bay Transportation Authority']
 
-    # TODO: Verify this has all the MA rta's
-    ma_riders = ridership_df.loc[ridership_df['UZA Name'].str.contains(', MA')]
-    ma_riders = ma_riders.loc[ma_riders['Agency'] != 'Massachusetts Bay Transportation Authority']
-    agency_set = list(set([stop['attributes']['Agency'] for stop in stops_data]))
+    bus_stop_income_df.to_csv('data/rta_bus_stop_income_ma.csv')
+    route_df.to_csv('data/rta_bus_route_ma.csv')
+    ridership_df.to_csv('data/rta_bus_ridership_ma.csv')
 
-    bus_area_income_df.to_csv('../data/bus_area_income_ma.csv')
-    route_df.to_csv('../data/rta_route_ma.csv')
-    ridership_df.to_csv('../data/rta_ridership_ma.csv')
+    map_stops_to_routes()
+    # join bus_stop_income_df and map_stops_to_routes to get income for every bus route
+    result = get_joined_data()
+    result.to_csv('data/result.csv')
     
     return {
         "route_df": route_df,
-        "population_df": population_df,
         "ridership_df": ridership_df,
-        "county_df": county_df,
-        "bus_area_income": bus_area_income_df,
-        "ma_riders": ma_riders,
-        "result": get_joined_data()
+        "tract_population_df": tract_population_df,
+        "county_population_df": county_population_df,
+        "bus_stop_income": bus_stop_income_df,
+        "result": result
     }
